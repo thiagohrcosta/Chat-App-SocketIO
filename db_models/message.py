b1272399e20c1dd78f4cbdc4e2ab316f3bf4f4e9
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-04-20 23:20:09 -0300

    Adding room creation

--- db_models/message.py
+++ db_models/message.py
@@ -1,6 +1,6 @@
 from repository.database import db
 
 class Message(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  message = db.Column(db.String(500), nullable=False)
-  room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(500), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
--- db_models/room.py
+++ db_models/room.py
@@ -1,6 +1,7 @@
 from repository.database import db
+from db_models.message import Message
 
 class Room(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), nullable=False)
-  messages = db.relationship('Message', backref='room', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    messages = db.relationship('Message', backref='room', lazy=True)
